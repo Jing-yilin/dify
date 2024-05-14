cda32150641bf4da41f4d2205d9e80ea07e38bf1
Jing-yilin <yilin.jing.ai@outlook.com>
2024-05-14 22:56:13 +0800

    ✨ feat: support .doc

--- api/core/helper/code_executor/jinja2_transformer.py
+++ api/core/helper/code_executor/jinja2_transformer.py
@@ -1,10 +1,7 @@
 import json
 import re
 from base64 import b64encode
-from typing import Optional
 
-from core.helper.code_executor.entities import CodeDependency
-from core.helper.code_executor.python_transformer import PYTHON_STANDARD_PACKAGES
 from core.helper.code_executor.template_transformer import TemplateTransformer
 
 PYTHON_RUNNER = """
